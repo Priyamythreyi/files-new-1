34938fc4f15f9ab1ea4308091642d8cafdf429a6
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-09-11 15:15:27 +0530

    Create practice 1.java

---
+++ practice 1.java
@@ -0,0 +1,11 @@
+public class Calculation{  
+	  void sum(int a,int b){System.out.println(a+b);}  
+	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+	  
+	  public static void main(String args[]){  
+	  Calculation obj=new Calculation();  
+	  obj.sum(20,20,20);  
+	  obj.sum(30,30);  
+	  
+	  }  
+	}  
