226f4f3d7c9edb061bc2c7e39928e7bb03d030fd
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-09-18 15:42:25 +0530

    Update practice 2i.java

--- practice 2i.java
+++ practice 2i.java
@@ -2,10 +2,10 @@
 public	class Bike8{  
 	 final int speedlimit=80;//final variable  
 	 void run(){  
-	   speed limit=300;  
+	  System.out.println("speed limit=300");
 	 }  
 	 public static void main(String args[]){  
 	 Bike8 obj=new  Bike8();  
 	 obj.run();  
     }  
-	}//end of class 
+	}//end of class  
