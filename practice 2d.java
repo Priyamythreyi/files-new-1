f00f0bfb431ce7400fe846e22cacfcf9ac4d1019
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-09-18 14:52:53 +0530

    Create practice 2d.java

---
+++ practice 2d.java
@@ -0,0 +1,13 @@
+//IS-A relationship
+public	class Employee{  
+	 float salary=60000; 
+	
+	}  
+	class Programmer extends Employee{  
+	 int bonus=5000;  
+	 public static void main(String args[]){  
+	   Programmer p=new Programmer();  
+	   System.out.println("Programmer salary is:"+p.salary);  
+	   System.out.println("Bonus of Programmer is:"+p.bonus);  
+	}  
+	}  
