0d1da1ed4a18990c2d3444365ede1204085afe87
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-09-18 15:38:23 +0530

    Create practice 2i.java

---
+++ practice 2i.java
@@ -0,0 +1,11 @@
+//Java final variable
+public	class Bike8{  
+	 final int speedlimit=80;//final variable  
+	 void run(){  
+	   speed limit=300;  
+	 }  
+	 public static void main(String args[]){  
+	 Bike8 obj=new  Bike8();  
+	 obj.run();  
+    }  
+	}//end of class 
