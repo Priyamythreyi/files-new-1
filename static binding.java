f7b1db99918269f6aa402a7e4c86c9ce5c96982c
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-10-08 14:58:02 +0530

    Create static binding.java

---
+++ static binding.java
@@ -0,0 +1,9 @@
+//static binding
+	public class cat{  
+	 private void eat(){System.out.println("cat is eating...");}  
+	  
+	 public static void main(String args[]){  
+	  cat d1=new cat();  
+	  d1.eat();  
+	 } 
+	}
