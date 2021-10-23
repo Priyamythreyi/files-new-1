1a14493c0fa2ab82628456983f77cb35621d104a
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-10-23 14:36:00 +0530

    Create single dimension array.java

---
+++ single dimension array.java
@@ -0,0 +1,17 @@
+//single dimensional java array
+	public class Testarray{  
+	public static void main(String args[]){  
+	  
+	int a[]=new int[6];//declaration and instantiation  
+	a[0]=30;//initialization  
+	a[1]=40;  
+	a[2]=60;  
+	a[3]=70;  
+	a[4]=80; 
+	a[5]=100;
+	  
+	//printing array  
+	for(int i=0;i<a.length;i++)//length is the property of array  
+	System.out.println(a[i]);  
+	  
+	}} 
