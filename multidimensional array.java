679f32dd6ccea0862e6310ddab3a543956f8d7f4
Priyamythreyi <89632129+Priyamythreyi@users.noreply.github.com>
2021-10-23 14:43:19 +0530

    Create multidimensional array.java

---
+++ multidimensional array.java
@@ -0,0 +1,16 @@
+//Multidimensional java array
+public	class Testarray3{  
+	public static void main(String args[]){  
+	  
+	//declaring and initializing 2D array  
+	int arr[][]={{6,8,10},{3,6,9},{8,8,6}};  
+	  
+	//printing 2D array  
+	for(int i=0;i<3;i++){  
+	 for(int j=0;j<3;j++){  
+	   System.out.print(arr[i][j]+" ");  
+	 }  
+	 System.out.println();  
+	}  
+	  
+	}} 
